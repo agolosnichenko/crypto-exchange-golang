fa2e27646c9919d9504be547b6ac425d9d18c51a
Anton Golosnichenko <a.golosnichenko@icloud.com>
2024-08-19 23:59:27 +0500

    feat: orderbook

--- orderbook_test.go
+++ orderbook_test.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestLimit(t *testing.T) {
+	l := NewLimit(10000)
+	buyOrderA := NewOrder(true, 5)
+	buyOrderB := NewOrder(true, 8)
+	buyOrderC := NewOrder(true, 10)
+
+	l.AddOrder(buyOrderA)
+	l.AddOrder(buyOrderB)
+	l.AddOrder(buyOrderC)
+
+	l.DeleteOrder(buyOrderB)
+
+	fmt.Println(l)
+}
+
+func TestOrderbook(t *testing.T) {
+	ob := NewOrderbook()
+
+	buyOrderA := NewOrder(true, 10)
+	buyOrderB := NewOrder(true, 2000)
+
+	ob.PlaceOrder(18000, buyOrderA)
+	ob.PlaceOrder(19000, buyOrderB)
+
+	fmt.Println(ob)
+
+	// for i := 0; i < len(ob.Bids); i++ {
+	// 	fmt.Println(ob.Bids[i])
+	// }
+}
