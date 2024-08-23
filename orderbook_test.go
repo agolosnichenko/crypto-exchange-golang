d13db21fab22c138141792315e83ffa983a83628
Anton Golosnichenko <a.golosnichenko@icloud.com>
2024-08-23 23:50:50 +0500

    feat: limit and market orders

--- orderbook_test.go
+++ orderbook_test.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestLimit(t *testing.T) {
@@ -20,18 +23,69 @@ func TestLimit(t *testing.T) {
 	fmt.Println(l)
 }
 
-func TestOrderbook(t *testing.T) {
+func TestPlaceLimitOrder(t *testing.T) {
 	ob := NewOrderbook()
 
-	buyOrderA := NewOrder(true, 10)
-	buyOrderB := NewOrder(true, 2000)
+	sellOrderA := NewOrder(false, 10)
+	sellOrderB := NewOrder(false, 5)
+	ob.PlaceLimitOrder(10000, sellOrderA)
+	ob.PlaceLimitOrder(9000, sellOrderB)
 
-	ob.PlaceOrder(18000, buyOrderA)
-	ob.PlaceOrder(19000, buyOrderB)
+	require.Equal(t, 2, len(ob.asks))
+}
 
-	fmt.Println(ob)
+func TestPlaceMarketOrder(t *testing.T) {
+	ob := NewOrderbook()
 
-	// for i := 0; i < len(ob.Bids); i++ {
-	// 	fmt.Println(ob.Bids[i])
-	// }
+	sellOrder := NewOrder(false, 20)
+	ob.PlaceLimitOrder(10000, sellOrder)
+
+	buyOrder := NewOrder(true, 10)
+	matches := ob.PlaceMarketOrder(buyOrder)
+
+	assert.Equal(t, 1, len(matches))
+	assert.Equal(t, 1, len(ob.asks))
+	assert.Equal(t, 10.0, ob.AskTotalVolume())
+	assert.Equal(t, sellOrder, matches[0].Ask)
+	assert.Equal(t, buyOrder, matches[0].Bid)
+	assert.Equal(t, 10.0, matches[0].SizeFilled)
+	assert.Equal(t, 10000.0, matches[0].Price)
+	assert.True(t, buyOrder.IsFilled())
+}
+
+func TestPlaceMarketOrderMultiFill(t *testing.T) {
+	ob := NewOrderbook()
+
+	buyOrderA := NewOrder(true, 5)
+	buyOrderB := NewOrder(true, 8)
+	buyOrderC := NewOrder(true, 10)
+	buyOrderD := NewOrder(true, 1)
+
+	ob.PlaceLimitOrder(5000, buyOrderC)
+	ob.PlaceLimitOrder(5000, buyOrderD)
+	ob.PlaceLimitOrder(9000, buyOrderB)
+	ob.PlaceLimitOrder(10000, buyOrderA)
+
+	assert.Equal(t, 24.0, ob.BidTotalVolume())
+
+	sellOrder := NewOrder(false, 20)
+	matches := ob.PlaceMarketOrder(sellOrder)
+
+	assert.Equal(t, 4.0, ob.BidTotalVolume())
+	assert.Equal(t, 3, len(matches))
+	assert.Equal(t, 1, len(ob.bids))
+
+	fmt.Println(matches)
+}
+
+func TestCancelOrder(t *testing.T) {
+	ob := NewOrderbook()
+	buyOrder := NewOrder(true, 4)
+	ob.PlaceLimitOrder(10000.0, buyOrder)
+
+	assert.Equal(t, 1, len(ob.bids))
+	assert.Equal(t, 4.0, ob.BidTotalVolume())
+
+	ob.CancelOrder(buyOrder)
+	assert.Equal(t, 0.0, ob.BidTotalVolume())
 }
